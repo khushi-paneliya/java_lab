e9c84323a312f2572e1f267e5b1869e9db611788
khushi-paneliya <paneliyakhushi777@gmail.com>
2026-07-10 08:23:41 +0530

    Add files via upload

---
+++ Test_Break.java
@@ -0,0 +1,15 @@
+class Test_Break
+{
+	public static void main (String args[])
+	{
+		int[]numbers={10,20,30,40,50};
+		for(int x:numbers)
+		{
+			if(x==30)
+			{
+				break;
+			}
+			System.out.println(x);
+		}
+	}
+}
